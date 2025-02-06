4a96a7206ea8a022d642346be96d26928bba868d
Adrian Q.A <adrianquispe515@gmail.com>
2025-02-06 12:21:21 -0500

    Fix: configuración de Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://adrianquispe16.github.io/portafolio-web/',
   base: '/portafolio-web/',
   trailingSlash: "always",
-  output: 'static'
+  output: 'static',
+  vite: {
+    build: {
+      assetsInlineLimit: 0, // ⬅️ Asegura que los assets no se inyecten como base64
+      rollupOptions: {
+        output: {
+          assetFileNames: "[name].[ext]"
+        }
+      }
+    }
+  }
 });
