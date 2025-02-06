87366c749480b6968375af709b2786c5a982dc37
Adrian Q.A <adrianquispe515@gmail.com>
2025-02-05 21:52:06 -0500

    astro deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx,vue,css}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			textSans: ['ui-sans-serif'],
+		},
+		extend: {
+			colors: {
+				rosa: '#CC0088',
+				colorCuerpo: '#16191d',
+				diaColor: '#F9FAFBA6',
+				colorOpciones: '#868e96',
+			},
+			width: {
+				tamañoCuerpo: '730px', 
+			},
+		},
+	},
+	  
 };
